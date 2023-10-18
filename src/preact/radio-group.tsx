23394af80e6afb26f9edf42e23d61d0ea9a4f958
nick thomas <nichoth@gmail.com>
2023-10-18 16:29:55 -0700

    wip

---
+++ src/preact/radio-group.tsx
@@ -0,0 +1,28 @@
+import { FunctionComponent } from 'preact'
+import kebabCase from 'just-kebab-case'
+import './radio-group.css'
+
+interface Props {
+    id?:string
+    name:string
+    legend:string
+    options:string[]
+    required:boolean
+}
+
+export const RadioGroup:FunctionComponent<Props> = function (props) {
+    const { name, options, legend, required, id } = props
+
+    return (<fieldset className="form-group radios">
+        <legend>{legend}</legend>
+
+        {options.map((opt, i) => {
+            return (<label key={i} className="radio-input">
+                <input type="radio" name={name} required={required} id={id}
+                    value={kebabCase(opt)}
+                />
+                {opt}
+            </label>)
+        })}
+    </fieldset>)
+}
